28cf31e8f900177bbc13faddf100f41c8ecdd51b
Radu Guran <radug314@gmail.com>
2020-07-22 21:01:00 +0300

    Added getRandomQuestion method. Added maven.compiler source/target to pom.xml

--- src/main/java/ro/jademy/millionaire/Main.java
+++ src/main/java/ro/jademy/millionaire/Main.java
@@ -1,44 +1,63 @@
 package ro.jademy.millionaire;
 
+import ro.jademy.millionaire.data.QuestionProvider;
 import ro.jademy.millionaire.model.Game;
 import ro.jademy.millionaire.model.Question;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
         //1. define objects
-            // define properties
-            // define methods
+        // define properties
+        // define methods
 
         //2. define interactions
 
         //initialize questions
 
         //List<Question> difficultyZeroQuestion = new ArrayList<>();
-        List<Question> difficultyZeroQuestion = getRandomQuestions(5,0);
-        List<Question> difficultyOneQuestion = getRandomQuestions(5,1);
-        List<Question> difficultyTwoQuestion = getRandomQuestions(4,2);
-        List<Question> difficultyThreeQuestion = getRandomQuestions(1,3);
+        List<Question> difficultyZeroQuestion = getRandomQuestions(5, 0);
+        List<Question> difficultyOneQuestion = getRandomQuestions(5, 1);
+        List<Question> difficultyTwoQuestion = getRandomQuestions(4, 2);
+        List<Question> difficultyThreeQuestion = getRandomQuestions(1, 3);
 
         //initialize game
-        Game game = new Game(difficultyZeroQuestion,difficultyOneQuestion,difficultyTwoQuestion,difficultyThreeQuestion);
+        Game game = new Game(difficultyZeroQuestion, difficultyOneQuestion, difficultyTwoQuestion, difficultyThreeQuestion);
 
         // TODO:
         //start game
         //game.start();
     }
 
-    private static List<Question> getRandomQuestions(int nrOfQuestions,int difficulty) {
+    private static List<Question> getRandomQuestions(int nrOfQuestions, int difficulty) {
         //TODO:
         //loop through all questions
         //get all questions of given difficulty
         //loop through sub-list until nrOfQuestions and select random items by index
         //return list
 
-        return new ArrayList<>();
+        List<Question> questionsDiff = new ArrayList<>();
+        List<Question> randomQuestions = new ArrayList<>();
+
+        for (Question question : QuestionProvider.ALL_QUESTIONS) {
+            if (question.getDifficulty() == difficulty) {
+                questionsDiff.add(question);
+            }
+        }
+
+        Random random = new Random();
+
+        for (int i = 0; i < nrOfQuestions; i++) {
+
+            int randomIndex = random.nextInt(questionsDiff.size());
+            randomQuestions.add(questionsDiff.remove(randomIndex));
+        }
+
+        return randomQuestions;
     }
 }
--- src/main/java/ro/jademy/millionaire/data/QuestionProvider.java
+++ src/main/java/ro/jademy/millionaire/data/QuestionProvider.java
@@ -9,153 +9,153 @@ import java.util.List;
 
 public class QuestionProvider {
 
-    private static List<Question> ALL_QUESTIONS = Arrays.asList(
+    public static List<Question> ALL_QUESTIONS = Arrays.asList(
 
-     new Question("Question 1?" , Arrays.asList(
-             new Answer("one"),
-             new Answer("two"),
-             new Answer("three")),
-
-             new Answer("four"), 0),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 0),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 0),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 0),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 0),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 0),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 0),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
+                    new Answer("one"),
+                    new Answer("two"),
+                    new Answer("three")),
+
+                    new Answer("four"), 0),
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 1),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 1),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 1),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 1),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 1),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 1),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 1),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 2),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 2),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 2),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 2),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 2),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 2),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 2),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 3),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 3),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
 
                     new Answer("four"), 3),
-            new Question("Question 1?" , Arrays.asList(
+            new Question("Question 1?", Arrays.asList(
                     new Answer("one"),
                     new Answer("two"),
                     new Answer("three")),
